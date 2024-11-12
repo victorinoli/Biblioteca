ba17f26af33f48bb26844c49f0832707fad7b4d8
victor oliveira <victor.in@icloud.com>
2024-11-12 18:12:35 -0300

    new project

--- src/Livro.java
+++ src/Livro.java
@@ -5,6 +5,57 @@ public class Livro {
     private String isbn;
     private boolean disponivel;
 
+    public Livro(String titulo, String autor, String isbn) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.disponivel = true;
+    }
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void locaLivro() {
+        if(disponivel) {
+            disponivel = false;
+            System.out.printf("Livro %s locado com sucesso!\n", titulo);
+        }else {
+            System.out.printf("Livro %s nao esta disponivel!\n", titulo);
+        }
+    }
+
+    public void devolveLivro() {
+        if(!disponivel) {
+            disponivel = true;
+            System.out.printf("Livro %s devolvido com sucesso!\n", titulo);
+        }else{
+            System.out.printf("Livro %s nao esta locado!\n", titulo);
+        }
+    }
 
 }
